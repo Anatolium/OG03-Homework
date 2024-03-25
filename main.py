5a626f9cbe759da728cfc27463dd405ce3a2823f
Анатолий <lan2002@yandex.ru>
2024-03-25 17:16:08 +0300

    img content fix

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
-#icon = pygame.image.load("img/tir-game-icon.png")
-#pygame.display.set_icon(icon)
+icon = pygame.image.load("img/tir-game-icon.png")
+pygame.display.set_icon(icon)
 
 target_image = pygame.image.load("img/target.png")
 target_width = 120
